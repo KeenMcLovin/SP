bf68119f73d8286ad67626aa092a3ef71ca3471d
KeenMcLovin <al221911456@gmail.com>
2020-10-25 23:48:34 -0600

    modelos y controladores

---
+++ BarberSkyn/app/Http/Controllers/OfertaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\oferta;
+use Illuminate\Http\Request;
+
+class OfertaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\oferta  $oferta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(oferta $oferta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\oferta  $oferta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(oferta $oferta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\oferta  $oferta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, oferta $oferta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\oferta  $oferta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(oferta $oferta)
+    {
+        //
+    }
+}
